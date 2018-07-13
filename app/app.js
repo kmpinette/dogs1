3e1e281dac5954d1c89e1240052657996215c111
kpinette <kmpinette@gmail.com>
2018-07-12 22:57:05 -0400

    http

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
